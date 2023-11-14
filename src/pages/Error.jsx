2471289c918778626d892042cbea73fbf17fa7b6
유애나 <77397206+Teerapat28@users.noreply.github.com>
2023-11-14 18:07:03 +0700

    Navbar Styled Components About Page Error Page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import { Link, useRouteError } from "react-router-dom";
+import img from "../assets/not-found.svg";
 const Error = () => {
-  return (
-    <div>Error</div>
-  )
-}
+  const error = useRouteError();
 
-export default Error
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>Ohh!</h3>
+          <p>we can't seem to find page you are looking for.</p>
+          <Link to="/"> back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <div>
+        <h3>something went wrong</h3>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Error;
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Link , Outlet} from "react-router-dom";
-
+import { Link, Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 const HomeLayout = () => {
   return (
-    <div>
-      <nav>Navbar</nav>
-      <Outlet/>
-    </div>
+    <>
+      <Navbar />
+      <section className="page">
+        <Outlet />
+      </section>
+    </>
   );
 };
 
